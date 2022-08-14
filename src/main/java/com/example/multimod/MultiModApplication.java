c8e0d4e4e5273b9280a07bb42c27b357b02804af
charatkakkar <charatkakkar@gmail.com>
2022-08-14 14:29:15 -0400

    Initial commit

---
+++ src/main/java/com/example/multimod/MultiModApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModApplication.class, args);
+	}
+
+}
